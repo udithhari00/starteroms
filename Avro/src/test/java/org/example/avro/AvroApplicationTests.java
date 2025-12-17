d370e3aa99b1ce8c8ec4b43a49ae2b1bd831f345
udithhari00 <udith.hari@acuverconsulting.com>
2025-12-17 12:00:47 +0530

    Avro

---
+++ Avro/src/test/java/org/example/avro/AvroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
