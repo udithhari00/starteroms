0c946cae6c2b9f175ff831bb7022faefabb4844f
udithhari00 <udith.hari@acuverconsulting.com>
2026-01-28 12:38:23 +0530

    Added payment db

--- order-service/init-postgres.sh
+++ order-service/init-postgres.sh
@@ -3,4 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     ALTER USER postgres WITH PASSWORD 'postgres';
+    CREATE DATABASE payments;
 EOSQL
